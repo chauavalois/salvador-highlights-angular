e9ee80508b43a398b39d298c316d9e7d4735d4d6
chauavalois <chauavalois@hotmail.com>
2022-03-09 22:47:11 -0300

    confecção-de-noticias.ts

---
+++ src/app/models/noticias.ts
@@ -0,0 +1,39 @@
+export interface Noticias {
+    noticia_int_id: number;
+            noticia_txt_titulo: string;
+            noticia_txt_texto: string;
+            noticia_txt_foto: string;
+            created_at: string;
+            updated_at: string;
+            deleted_at: null;
+            noticia_bool_ativo: number;
+            noticia_tipos: [
+                {
+                    noticiaTipo_int_id: number;
+                    noticiaTipo_txt_chave: string;
+                    noticiaTipo_txt_valor: string
+                    noticiaTipo_bool_ativo: number;
+                    deleted_at: null;
+                    created_at: null;
+                    updated_at: null;
+                    pivot: {
+                        noticia_int_id: number;
+                        noticiaTipo_int_id: number
+                    }
+                },
+                {
+                    noticiaTipo_int_id: number;
+                    noticiaTipo_txt_chave: string;
+                    noticiaTipo_txt_valor: string;
+                    noticiaTipo_bool_ativo: number;
+                    deleted_at: null;
+                    created_at: null;
+                    updated_at: null;
+                    pivot: {
+                        noticia_int_id: number;
+                        noticiaTipo_int_id: number
+                    }
+                }
+            ]
+        }
+
