ca3906f1f0d526a7b025e3916cd6a14447b54b20
chauavalois <chauavalois@hotmail.com>
2022-04-19 06:28:00 -0300

    correções e criação do component adimin

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { INoticia } from '../models/noticias';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { FormsModule } from '@angular/forms';
     ContatosComponent,
     HeaderComponent,
     FooterComponent,
+    AdminComponent,
     ],
   imports: [
     BrowserModule,
