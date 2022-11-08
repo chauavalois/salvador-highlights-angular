34e7eeceda983f1f349f6c81d2e8f12f263cca28
chauavalois <youremail@domain.comchauavalois@hotmail.com>
2022-11-08 07:16:39 -0300

    pagina de criacao e edicao preparada para receber a logica

--- src/app/admin/editar-noticia/editar-noticia.component.ts
+++ src/app/admin/editar-noticia/editar-noticia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { INoticia } from 'src/app/models/noticias';
 
 @Component({
   selector: 'app-editar-noticia',
@@ -8,6 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class EditarNoticiaComponent implements OnInit {
 
   constructor() { }
+
+  public editarNoticia: INoticia = {
+    noticia_int_id: 0,
+    noticia_txt_titulo: "",
+    noticia_txt_texto: "",
+    noticia_txt_foto: "",
+    created_at: "",
+    updated_at: "",
+    deleted_at: "",
+    noticia_bool_ativo: false,
+    noticia_tipos: [
+        {
+            noticiaTipo_int_id: 0,
+            noticiaTipo_txt_chave: "",
+            noticiaTipo_txt_valor: "",
+            noticiaTipo_bool_ativo: false,
+            deleted_at: "",
+            created_at: "",
+            updated_at: "",
+
+        }
+
+    ]
+};
 
   ngOnInit(): void {
   }
