5c260188ff15044f47af40e76cb328e0d539baa8
chauavalois <chauavalois@hotmail.com>
2024-07-03 09:20:42 -0300

    feat: rota com guard para admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -6,14 +5,13 @@ import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 import { ContatosComponent } from './contatos/contatos.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'contatos', component: ContatosComponent},
-  {path: 'admin', component: AdminComponent},
-  {path: 'login', component: LoginComponent},
-
-
+  { path: '', component: HomeComponent },
+  { path: 'contatos', component: ContatosComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
