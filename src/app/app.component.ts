8b3e0b749ce3e9f6e2da9f264ea28cde57913241
chauavalois <chauavalois@hotmail.com>
2024-07-17 12:45:27 -0300

    refactor: retirando o rodapé das páginas admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'salvador-highlights-angular';
+export class AppComponent implements OnInit {
+  showFooter: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {  // Use `any` type here to avoid type mismatch
+      this.showFooter = !this.isAdminRoute(event.urlAfterRedirects);
+    });
+  }
+
+  isAdminRoute(url: string): boolean {
+    const adminRoutes = ['/admin', '/usuarios', '/editar-home', '/editar-sobre'];
+    return adminRoutes.some(route => url.startsWith(route));
+  }
 }
