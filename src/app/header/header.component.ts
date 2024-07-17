6a8442d156cc1868e32af20e11b8ce39acd54227
chauavalois <chauavalois@hotmail.com>
2024-07-17 12:55:40 -0300

    test: tentativa de esconder o link admin para usuarios nao logados

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  isAdmin = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.authService.isAdmin$.subscribe(isAdmin => {  //esse código seria para esconder o link admin apenas para usuários deslogados, mas não funcionou ainda.
+      this.isAdmin = isAdmin;
+    });
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }
