9f2cdedd67165728dc7434dc6d904a72f2d3318f
chauavalois <chauavalois@hotmail.com>
2024-07-17 12:44:29 -0300

    feat: rotas para novos components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { ContatosComponent } from './contatos/contatos.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { EditHomeComponent } from './edit-home/edit-home.component';
+import { EditAboutComponent } from './edit-about/edit-about.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'contatos', component: ContatosComponent },
-  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+  { path: 'admin', component: AdminDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'usuarios', component: UserListComponent, canActivate: [AuthGuard] },
+  { path: 'editar-home', component: EditHomeComponent, canActivate: [AuthGuard] },
+  { path: 'editar-sobre', component: EditAboutComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent } // Adicionando a rota de registro
+  { path: 'register', component: RegisterComponent } 
 ];
 
 @NgModule({
